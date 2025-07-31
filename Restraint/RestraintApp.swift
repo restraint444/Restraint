529fcd2980edd5b4cc0ec712c8aeca4cef74843b
restraint444 <restraintrocks@gmail.com>
2025-07-31 13:44:32 +1000

    Initial Commit

---
+++ Restraint/RestraintApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestraintApp.swift
+//  Restraint
+//
+//  Created by Ethan Tan on 31/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct RestraintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
